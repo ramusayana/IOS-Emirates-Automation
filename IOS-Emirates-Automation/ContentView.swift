46505e38fc28594756a24292abd362f99b15682c
ramusayana <ramusayana@gmail.com>
2025-12-03 16:45:35 +0000

    Initial Commit

---
+++ IOS-Emirates-Automation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS-Emirates-Automation
+//
+//  Created by Kodanda Ram on 03/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS-Emirates-Automation/IOS_Emirates_AutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Emirates_AutomationApp.swift
+//  IOS-Emirates-Automation
+//
+//  Created by Kodanda Ram on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Emirates_AutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
